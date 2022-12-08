e73b469ad5cd0da728d05e477767d8fa2020dec4
Indra <wuppie55@gmail.com>
2022-12-08 21:58:20 +0100

    Add day 8 part 2

--- Day8.cs
+++ Day8.cs
@@ -20,18 +20,22 @@ class Day8 : IDay
 
     public object Part2(List<string> lines)
     {
-        return null;
+        var score = 0;
+
+        for (var i = 0; i < lines.Count; i++)
+        {
+            for (var j = 0; j < lines[i].Length; j++)
+            {
+                CheckVisibility(lines, i, j, out var scene);
+
+                if (scene > score) score = scene;
+            }
+        }
+
+        return score;
     }
-    
-    /**
-            y----
-        x
-        |
-        |
 
-    */
-
-    bool CheckVisibility(List<string> lines, int x, int y)
+    private bool CheckVisibility(List<string> lines, int x, int y)
     {
         // check edge
         if (x == 0 || y == 0 || x == lines.Count - 1 || y == lines[0].Length - 1)
@@ -39,32 +43,56 @@ class Day8 : IDay
             return true;
         }
 
+        return CheckVisibility(lines, x, y, out var _);
+    }
+
+    private bool CheckVisibility(List<string> lines, int x, int y, out int score)
+    {
         var height = lines[x][y];
         var hidden = 0;
 
+        score = 0;
+        var trees = 0;
+
         // up
-        for (int i = x - 1; i >= 0; i--)
+        for (var i = x - 1; i >= 0; i--)
         {
+            trees++;
             if (lines[i][y] >= height) { hidden++; break; }
         }
+
+        score = trees;
+        trees = 0;
 
         // down
-        for (int i = x + 1; i < lines.Count; i++)
+        for (var i = x + 1; i < lines.Count; i++)
         {
+            trees++;
             if (lines[i][y] >= height) { hidden++; break; }
         }
 
+        score *= trees;
+        trees = 0;
+
         // left
-        for (int i = y - 1; i >= 0; i--)
+        for (var i = y - 1; i >= 0; i--)
         {
+            trees++;
             if (lines[x][i] >= height) { hidden++; break; }
         }
 
+        score *= trees;
+        trees = 0;
+
         // right
-        for (int i = y + 1; i < lines[0].Length; i++)
+        for (var i = y + 1; i < lines[0].Length; i++)
         {
+            trees++;
             if (lines[x][i] >= height) { hidden++; break; }
         }
+
+        score *= trees;
+        trees = 0;
 
         return hidden != 4;
     }
